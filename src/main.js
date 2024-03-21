2e5ad5d546847ccc118f82020f46b688bda59058
bryantmo <gn4623GNA>
2024-03-21 17:26:07 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -4,6 +4,3 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
-
-<p>Using text interpolation: {{ rawHtml }}</p>
-<p>Using v-html directive: <span v-html="rawHtml"></span></p>
