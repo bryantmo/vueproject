90d01b6b803590f411de50409121eb20b58fa327
bryantmo <gn4623GNA>
2024-03-22 10:17:22 +0800

    手撸一次src/App.vue和src/main.ts

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <!-- html 结构 -->
+    <div class="app2">
+        <h1>hello bryant</h1>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+    // JS/TS
+    export default {
+        name:'App bryant' //组件名
+    }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+    .app2 {
+        background-color: #ddd;
+        box-shadow: 0 0 10px;
+        border-radius: 90px;
+        padding: 20px;
+    }
 </style>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// 引入createApp
+import {createApp} from 'vue'
+// app根组件
+import App from './App.vue'
+
+createApp(App).mount('#app')
