4ea546c8e212ae3c2bcac09cf00594f5db279a12
bryantmo <18819467607@163.com>
2024-04-25 23:30:29 +0800

    ts 接口泛型

---
+++ backup/16、TS接口泛型和规范/types/index.ts
@@ -0,0 +1,9 @@
+// 定义一个接口，用于限制Person对象的具体属性
+export interface PersonInter {
+    id:string,
+    name:string,
+    age:number
+}
+// 一个自定义类型
+export type PersonInterList = Array<PersonInter>
+export type PersonInters = PersonInter[]
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <template>
     <!-- html 标签 -->
-    <h2 class="person" ref="title2">你和</h2>
-    <button @click="showLog">点我输出App组件的h2元素</button>
-    <!-- 组件 标签 -->
-    <Person ref="ren"></Person>
+    <Person></Person>
 </template>
 
 <script lang="ts" setup name="App">
     import Person from './components/Person.vue'
-    import {ref} from 'vue'
 
-    let title2 = ref()
-    let ren = ref()
-
-    function showLog() {
-        console.log("App组件:", title2.value)
-        console.log("ref加到组件标签里:",ren.value)
-    }
 </script>
 
 <style>
