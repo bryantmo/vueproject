420d0821be7b07b3b2e07708650b0772077885a9
bryantmo <18819467607@163.com>
2024-05-14 23:59:46 +0800

    自定义狗子

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,51 +2,36 @@
     <div class="person">
         <h2>当前求和为：{{sum}}</h2>
         <button @click="add">点我sum+1</button>
+        <hr>
+        <img v-for="(dog, index) in dogList" :src="dog" :key="index">
+        <button @click="getDog">再来一只小狗</button>
     </div>
 </template>
 
 <script setup name="Person" lang="ts">
     import {ref, onBeforeMount, onMounted,
         onBeforeUpdate, onUpdated,
-        onBeforeUnmount, onUnmounted} from 'vue'
+        onBeforeUnmount, onUnmounted, reactive} from 'vue'
+    import axios from 'axios'
 
     //数据
     let sum = ref(0)
+    let dogList = reactive([
+        'https://images.dog.ceo//breeds//pembroke//n02113023_9001.jpg',
+        'https://images.dog.ceo//breeds//pembroke//n02113023_4611.jpg'
+    ])
 
     // 方法
     function add() {
         sum.value += 1
     }
 
-    // 生命周期 - 创建； beforeCreate created => 被setup函数取代
-    console.log("创建好子组件...")
-
-    // 生命周期 - 挂载：onBeforeMount, onBeforeUpdate
-    onBeforeMount(() => {
-        console.log("子挂载前函数，手动调用一次onBeforeMount，是vue合适的时候调用的")
-    })
-
-    onMounted(() => {
-        console.log("子挂载完毕函数")
-    })
-
-    // 生命周期 - 更新：onBeforeUpdate, onUpdated
-    onBeforeUpdate(() => {
-        console.log("子更新前")
-    })
-
-    onUpdated(() => {
-        console.log("子更新完毕")
-    })
-
-    // 生命周期 - 卸载：
-    onBeforeUnmount(() => {
-        console.log("子卸载前")
-    })
-
-    onUnmounted(() => {
-        console.log("子卸载完毕")
-    })
+    // 方法
+    async function getDog() {
+        let result = await axios.get('https://dog.ceo/api/breed/pembroke/images/random')
+        console.log(result.data)
+        dogList.push(result.data.message)
+    }
 
 </script>
 
@@ -63,5 +48,11 @@
     }
     li{
         font-size: 20px;
+    }
+
+    img {
+        /* width: 100px; */
+        height: 100px;
+        margin-left: 10px;
     }
 </style>
